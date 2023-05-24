7c49cb5ac7feca9087a6f6efee1b97bfca6c4f26
ItsZeusX <youssefelkhattabe1@gmail.com>
2023-05-24 14:10:52 -0700

    Shell.c

--- shell.c
+++ shell.c
@@ -1,83 +1,49 @@
-#include "shell.h"
-
-int status = 0;
-int lune_num = 1;
-char *shell_name = NULL;
+#include "main.h"
 
 /**
- * main - executes commands froù the terminal
- * @ac: number of inputs from main
- * @av: array of inputs from main
- * Return: 0, or another number if desired
+ * tokenize - breaks command line args into words
+ * @av: is the args vector
+ * @word_count: is the number of words
+ * @lineptr_copy: is the copy of the input args
+ * @word: broken down command args
  */
-int main(__attribute__((unused))int ac, char **av)
+int tokenize(char **av, int word_count, char *lineptr_copy, char *word)
 {
-	int bytes_read;
-	int is_separated = FALSE;
+	char *terminalcmd = NULL;
+
 	int i;
-	size_t buf_size = 1;
-	char *buf = NULL;
-	char *buf_ptr;
-	char *buf_tmp;
-	char **args = NULL;
 
-	shell_name = _strdup(*av);
-	environ = array_cpy(environ, list_len(environ, NULL));
-	signal(SIGINT, SIG_IGN);
+	pid_t child;
 
-	buf  = malloc(1);
-	if (buf == NULL)
-		exit(EXIT_FAILURE);
+	av = malloc(sizeof(char *) * word_count);
 
-	while (1)
+	word = strtok(lineptr_copy, " \n");
+	for (i = 0; word != NULL; i++)
 	{
-		if (is_separated == FALSE)
-		{
-			if (isatty(STDIN_FILENO) == 1)
-				write(STDOUT_FILENO, "my_shell$ ",10);
-
-			bytes_read = getline(&buf, &buf_size, stdin);
-
-			if (bytes_read == -1)
-				break;
-			if (bytes_read ==1)
-			{
-				line_num++;
-				continue;
-			}
-			buf[bytes_read - 1] = '\0';
-			buf = input_san(buf, &buf_size);
-			if (buf_size == 0)
-			{
-				line_num++;
-				continue;
-			}
-			buf_ptr = buf;
-		}
-		else
-			buf_ptr = buf_tmp;
-
-		buf_tmp = NULL;
-		args = make_array(buf_ptr, ' ',&buf_tmp);
-		if (buf_tmp != NULL)
-			is_separated = TRUE;
-		else
-			is_separated = FALSE;
-
-		i = command_manager(args);
-
-		free(args);
-
-		if (is_separated == FALSE)
-			line_num++;
-
-		if (i == EXIT_SHELL)
-			break;
+		av[i] = malloc(sizeof(char) * _strlen(word));
+		_strcpy(av[i], word);
+		word = strtok(NULL, " \n");
 	}
-	free(buf);
-	alias_func(NULL, TRUE);
-	free_array(environ);
-	free(shell_name);
+	av[i] = NULL;
 
-	return (status % 256);
+	terminalcmd = handle_path(av[0]);
+
+	child = fork();
+	if (child == -1)
+	{
+		perror("Error");
+		return (1);
+	}
+	if (child == 0)
+	{
+		if (execve(terminalcmd, av, NULL) == -1)
+		{
+			perror("Error");
+		}
+	}
+	else
+	{
+		wait(NULL);
+	}
+	return (0);
 }
